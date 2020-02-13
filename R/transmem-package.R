803dae70a20ab1154d3c471f426dce8cf7f58462
Crparedes <craparedesca@unal.edu.co>
2020-02-13 16:57:12 -0600

    readme file created

---
+++ R/transmem-package.R
@@ -0,0 +1,21 @@
+#' transmem: General tools to manipulate and visualize membrane processes data.
+#'
+#' This package provides tools for treating and visualizing the results
+#' from different experiments involving (selective) transport across membranes.
+#' Transport profiles involving up to three species can be easily obtained
+#' as publication-ready plots, several membrane performance parameters can be
+#' calculated and many experimental results can be easily studied through the
+#' package's graphical representations. The package allows the automatization
+#' of many routinary data-treatment tasks allowing the experimenter to focus
+#' on the more important issues of the research.
+#'
+#' @author Cristhian Paredes, \email{craparedesca@@unal.edu.co}
+#' @author Eduardo Rodriguez de San Miguel, \email{erdsmg@@unam.mx}
+#' @references
+#'   C. Paredes, E. Rodriguez de San Miguel, Polymer inclusion membrane for
+#'   the recovery and concentration of lithium from seawater. Master thesis,
+#'   Universidad Nacional Autónoma de México, México City, México, 2020.
+#'
+#' @docType package
+#' @name transmem-package
+NULL
